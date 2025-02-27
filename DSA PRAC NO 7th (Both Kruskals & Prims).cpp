9d51570910306fa64229c34f09048021d67c0f5c
Alok Bhorunde <124031666+alokbhorunde9814@users.noreply.github.com>
2025-02-27 20:42:33 +0530

    Update DSA PRAC NO 7th (Both Kruskals & Prims).cpp

--- DSA PRAC NO 7th (Both Kruskals & Prims).cpp
+++ DSA PRAC NO 7th (Both Kruskals & Prims).cpp
@@ -1,4 +1,4 @@
-/*#PROVIDED BY mr.SHLOK
+/*
 7. Represent a graph of your college campus using adjacency list /adjacency matrix. Nodes should represent
 the various departments/institutes and links should represent the distance between them. Find minimum
 spanning tree
